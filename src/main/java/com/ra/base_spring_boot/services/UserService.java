f18345a30c6d57a2c8c436a11930ec65a7832aec
Lê Khánh Dương <lekhanhduong.contact@gmail.com>
2026-04-20 08:57:48 +0700

    Revert "Feat: Task List Users Admin"

--- src/main/java/com/ra/base_spring_boot/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.ra.base_spring_boot.services;
-
-import com.ra.base_spring_boot.dto.response.AccountResponseDTO;
-import org.springframework.data.domain.Page;
-
-public interface UserService {
-    Page<AccountResponseDTO> getStudentList(String search, int page, int size, String sortBy, String direction);
-}
